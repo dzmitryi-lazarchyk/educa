0ffc854a2979512946bcf97d8da86fb06cb75fb2
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2024-02-25 18:51:11 +0300

    Managing modules and their contents.

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('content/<int:id>/delete/',
          views.ContentDeleteView.as_view(),
          name='module_content_delete'),
-    path('module/<id:module_id>/',
+    path('module/<int:module_id>/',
          views.ModuleContentListView.as_view(),
          name='module_content_list')
 ]
