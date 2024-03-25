e5a23ac2219c023550b498ee822a1f973e6a3349
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2024-03-25 04:24:15 +0300

    Setting up cache system. Caching Subjrcts in CourseListView.

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -11,6 +11,7 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from django.urls import reverse_lazy
 from django.db.models import Count
+from django.core.cache import cache
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
@@ -190,8 +191,13 @@ class CourseListView(TemplateResponseMixin, View):
     template_name = 'courses/course/list.html'
 
     def get(self, request, subject=None):
-        subjects = Subject.objects.annotate(
-            total_courses=Count('courses'))
+        subjects = cache.get('all_subjects')
+        if not subjects:
+            subjects = Subject.objects.annotate(
+                total_courses=Count('courses')
+            )
+            cache.set('all_subjects', subjects)
+
         courses = Course.objects.annotate(
             total_modules=Count('modules'))
 
