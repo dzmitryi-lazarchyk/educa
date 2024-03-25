010c510ca155990b12e7fa1a18a2d43e167b4eb4
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2024-03-25 04:58:19 +0300

    Caching Courses in CourseListView.

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'embed_video'
+    'embed_video',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,7 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
